2f0dbbce55f8848e0bce9500c93a45621f5fe50e
Paul Hawke <paul.hawke@gmail.com>
2022-11-17 15:26:33 -0600

    Cleanup on the way toward a KTS build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("7.3.1").apply(false)
     id("com.android.library").version("7.3.1").apply(false)
+    id("com.github.triplet.play").version("2.7.2").apply(false)
     kotlin("android").version("1.7.21").apply(false)
     kotlin("multiplatform").version("1.7.21").apply(false)
 }
